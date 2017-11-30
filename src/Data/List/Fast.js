a8a48f559e312e9b6b5bcd47500826e17d701fcb
Matthew Leon <ml@matthewleon.com>
2017-11-29 20:19:14 -0500

    wip

--- src/Data/List/Fast.js
+++ src/Data/List/Fast.js
@@ -118,3 +118,162 @@ exports.zipWith = function(f) {
     }
   }
 }
+
+exports.mapMaybeImpl = function(isJust, fromJust, f, l) {
+  var Nil = require('Data.List.Types').Nil;
+  var Cons = require('Data.List.Types').Cons;
+
+  if (l === Nil.value) {
+    return l;
+  } else {
+    var result = new Nil();
+
+    var input = l;
+    var output = result;
+
+    while (true) {
+      var maybeResult = f(input.value0);
+      if (isJust(maybeResult)) {
+        if (result === Nil.value) {
+          // Allocate a cons cell
+          var result = new Cons(null, null);
+        }
+        // Fill in the cons cell and allocate the next one
+        output.value0 = fromJust(maybeResult);
+        output.value1 = new Cons(null, null);
+
+        if (input.value1 === Nil.value) {
+          break;
+        }
+
+        input = input.value1;
+        output = output.value1;
+      }
+    }
+
+    if (result !== Nil.value) {
+      // Fill in the last remaining reference with Nil
+      output.value1 = Nil.value;
+    }
+
+    return result;
+  }
+};
+
+exports.partitionImpl = function(f) {
+  var Nil = require('Data.List.Types').Nil;
+  var Cons = require('Data.List.Types').Cons;
+
+  return function (l) {
+    // same logic as filter, but with two output lists
+    var left = Nil();
+    var right = Nil();
+
+    if (l !== Nil.value) {
+      var input = l;
+
+      var outLeft;
+      var outRight;
+      while (true) {
+        var outList;
+        var boolResult = f(input.value0);
+
+        if (boolResult === false) {
+          if (left === Nil.value) {
+            left = new Cons(null, null);
+            outLeft = left;
+          }
+          outLeft.value0 = boolResult;
+
+          if (input.value1 === Nil.value) {
+            break;
+          }
+
+          outLeft.value1 = new Cons(null, null)
+          outLeft = outLeft.value1;
+        } else {
+          if (right === Nil.value) {
+            right = new Cons(null, null);
+            outRight = right;
+          }
+          outRight.value0 = fromRight(eitherResult);
+
+          if (input.value1 === Nil.value) {
+            break;
+          }
+          outRight.value1 = new Cons(null, null);
+          outRight = outRight.value1;
+        }
+
+        input = input.value1;
+      }
+
+      // Fill in the last remaining reference with Nil
+      if (left !== Nil.value) {
+        outLeft.value1 = Nil.value;
+      }
+      if (right !== Nil.value) {
+        outRight.value1 = Nil.value;
+      }
+    }
+    return {left: left, right: right};
+  };
+};
+
+exports.partitionMapImpl = function(isLeft, fromLeft, fromRight, l) {
+  var Nil = require('Data.List.Types').Nil;
+  var Cons = require('Data.List.Types').Cons;
+
+  // same logic as mapMaybeImpl, but with two output lists
+  var left = Nil();
+  var right = Nil();
+
+  if (l !== Nil.value) {
+    var input = l;
+
+    var outLeft;
+    var outRight;
+    while (true) {
+      var outList;
+      var eitherResult = f(input.value0);
+
+      if (isLeft(eitherResult)) {
+        if (left === Nil.value) {
+          left = new Cons(null, null);
+          outLeft = left;
+        }
+        outLeft.value0 = fromLeft(eitherResult);
+
+        if (input.value1 === Nil.value) {
+          break;
+        }
+
+        outLeft.value1 = new Cons(null, null)
+        outLeft = outLeft.value1;
+      } else {
+        if (right === Nil.value) {
+          right = new Cons(null, null);
+          outRight = right;
+        }
+        outRight.value0 = fromRight(eitherResult);
+
+        if (input.value1 === Nil.value) {
+          break;
+        }
+        outRight.value1 = new Cons(null, null);
+        outRight = outRight.value1;
+      }
+
+      input = input.value1;
+    }
+
+    // Fill in the last remaining reference with Nil
+    if (left !== Nil.value) {
+      outLeft.value1 = Nil.value;
+    }
+    if (right !== Nil.value) {
+      outRight.value1 = Nil.value;
+    }
+  }
+  return {left: left, right: right};
+};
--- src/Data/List/Fast.purs
+++ src/Data/List/Fast.purs
@@ -3,11 +3,88 @@
 
 module Data.List.Fast where
 
+import Prelude hiding (map)
+
+import Control.Alt (class Alt)
+import Control.Alternative (class Alternative)
+import Control.Extend (class Extend)
+import Control.MonadPlus (class MonadPlus)
+import Control.MonadZero (class MonadZero)
+import Control.Plus (class Plus)
+import Data.Eq (class Eq1)
+import Data.Filterable (class Filterable)
+import Data.Foldable (class Foldable)
 import Data.Function.Uncurried (Fn2)
 import Data.List (List)
+import Data.Maybe (Maybe, isJust)
+import Data.Monoid (class Monoid)
+import Data.Newtype (class Newtype)
+import Data.Ord (class Ord1)
+import Data.Traversable (class Traversable)
+import Data.Unfoldable (class Unfoldable)
+import Data.Witherable (class Witherable)
+
+newtype FastList a = FastList (List a)
+derive instance newtypeFastList :: Newtype (FastList a) _
+derive newtype instance showFastList :: Show a => Show (FastList a)
+derive newtype instance eqFastList :: Eq a => Eq (FastList a)
+derive newtype instance eq1FastList :: Eq1 FastList
+derive newtype instance ordFastList :: Ord a => Ord (FastList a)
+derive newtype instance ord1FastList :: Ord1 FastList
+derive newtype instance semigroupFastList :: Semigroup (FastList a)
+derive newtype instance monoidFastList :: Monoid (FastList a)
+derive newtype instance foldableFastList :: Foldable FastList
+derive newtype instance unfoldableFastList :: Unfoldable FastList
+derive newtype instance traversableFastList :: Traversable FastList
+derive newtype instance applyFastList :: Apply FastList
+derive newtype instance applicativeFastList :: Applicative FastList
+derive newtype instance bindFastList :: Bind FastList
+derive newtype instance monadFastList :: Monad FastList
+derive newtype instance altFastList :: Alt FastList
+derive newtype instance plusFastList :: Plus FastList
+derive newtype instance alternativeFastList :: Alternative FastList
+derive newtype instance monadZeroFastList :: MonadZero FastList
+derive newtype instance monadPlusFastList :: MonadPlus FastList
+derive newtype instance extendFastList :: Extend FastList
+derive newtype instance witherableFastList :: Witherable FastList
+
+instance functorFastList :: Functor FastList where
+  map f (FastList xs) = FastList (map f xs)
+
+instance filterableFastList :: Filterable FastList where
+  filter = filter
+  filterMap = mapMaybe
+  partition = partitionImpl
+  partitionMap = partitionMapImpl isLeft fromLeft fromRight
 
 foreign import map :: forall a b. (a -> b) -> List a -> List b
 
 foreign import filter :: forall a. (a -> Boolean) -> List a -> List a
+
+mapMaybe :: forall a b. (a -> Maybe b) -> FastList a -> FastList b
+mapMaybe = runFn3 filterMapImpl isJust fromJust
+
+foreign import mapMaybeImpl
+  :: forall a b
+   . Fn4 (Maybe b -> Boolean)
+         (Maybe b -> b)
+         (a -> Maybe b)
+         FastList a
+         FastList b
+
+foreign import partitionImpl
+  :: forall a
+   . (a -> Boolean)
+  -> FastList a
+  -> { no :: FastList a, yes :: FastList b }
+   
+
+foreign import partitionMapImpl
+  :: forall a l r
+   . Fn5 (Either a b -> Boolean)
+         (Either a b -> a)
+         (Either a b -> b)
+         FastList a
+         { left :: FastList l, right :: FastList r}
 
 foreign import zipWith :: forall a b c. Fn2 a b c -> List a -> List b -> List c
