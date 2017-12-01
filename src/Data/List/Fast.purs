a218f8eca6e4b1fe7a79cf33aca87e1cdd4c79ee
Matthew Leon <ml@matthewleon.com>
2017-12-01 15:52:24 -0500

    building

--- src/Data/List/Fast.purs
+++ src/Data/List/Fast.purs
@@ -2,144 +2,98 @@
 -- | which exploit mutation under the hood.
 
 module Data.List.Fast
-  ( module Data.List.Types
-  , toUnfoldable
-  , fromFoldable
-
-  , singleton
-  , (..), range
-  , some
-  , someRec
-  , many
-  , manyRec
-
-  , null
-  , length
-
-  , snoc
-  , insert
-  , insertBy
-
-  , head
-  , last
-  , tail
-  , init
-  , uncons
-  , unsnoc
-
-  , (!!), index
-  , elemIndex
-  , elemLastIndex
-  , findIndex
-  , findLastIndex
-  , insertAt
-  , deleteAt
-  , updateAt
-  , modifyAt
-  , alterAt
-
-  , reverse
-  , concat
-  , concatMap
-  , filter
-  , filterM
-  , mapMaybe
-  , catMaybes
-  , mapWithIndex
-
-  , sort
-  , sortBy
-
-  , Pattern(..)
-  , stripPrefix
-  , slice
-  , take
-  , takeEnd
-  , takeWhile
-  , drop
-  , dropEnd
-  , dropWhile
-  , span
-  , group
-  , group'
-  , groupBy
-  , partition
-
-  , nub
-  , nubBy
-  , union
-  , unionBy
-  , delete
-  , deleteBy
-  , (\\), difference
-  , intersect
-  , intersectBy
-
-  , zipWith
-  , zipWithA
-  , zip
-  , unzip
-
-  , transpose
-
-  , foldM
-
-  , module Exports
+  ( FastList(..)
   ) where
 
 import Prelude hiding (map)
 
-import Data.List as L
-import Data.Newtype (unwrap)
+import Control.Alt (class Alt)
+import Control.Alternative (class Alternative)
+import Control.Extend (class Extend)
+import Control.MonadPlus (class MonadPlus)
+import Control.MonadZero (class MonadZero)
+import Control.Plus (class Plus)
+import Data.Either (Either, fromLeft, fromRight, isLeft)
+import Data.Eq (class Eq1)
+import Data.Filterable (class Filterable)
+import Data.Foldable (class Foldable)
+import Data.Function.Uncurried (Fn2, Fn4, Fn5, runFn4, runFn5)
+import Data.List (List)
+import Data.Maybe (Maybe, fromJust, isJust)
+import Data.Monoid (class Monoid)
+import Data.Newtype (class Newtype)
+import Data.Ord (class Ord1)
+import Data.Traversable (class Traversable)
+import Data.Unfoldable (class Unfoldable)
+import Data.Witherable (class Witherable)
+import Partial.Unsafe (unsafePartial)
 
-toUnfoldable :: forall f. Unfoldable f => FastList ~> f
-toUnfoldable = L.toUnfoldable <<< unwrap
+newtype FastList a = FastList (List a)
+derive instance newtypeFastList :: Newtype (FastList a) _
+derive newtype instance showFastList :: Show a => Show (FastList a)
+derive newtype instance eqFastList :: Eq a => Eq (FastList a)
+derive newtype instance eq1FastList :: Eq1 FastList
+derive newtype instance ordFastList :: Ord a => Ord (FastList a)
+derive newtype instance ord1FastList :: Ord1 FastList
+derive newtype instance semigroupFastList :: Semigroup (FastList a)
+derive newtype instance monoidFastList :: Monoid (FastList a)
+derive newtype instance foldableFastList :: Foldable FastList
+derive newtype instance unfoldableFastList :: Unfoldable FastList
+derive newtype instance traversableFastList :: Traversable FastList
+derive newtype instance applyFastList :: Apply FastList
+derive newtype instance applicativeFastList :: Applicative FastList
+derive newtype instance bindFastList :: Bind FastList
+derive newtype instance monadFastList :: Monad FastList
+derive newtype instance altFastList :: Alt FastList
+derive newtype instance plusFastList :: Plus FastList
+derive newtype instance alternativeFastList :: Alternative FastList
+derive newtype instance monadZeroFastList :: MonadZero FastList
+derive newtype instance monadPlusFastList :: MonadPlus FastList
+derive newtype instance extendFastList :: Extend FastList
+derive newtype instance witherableFastList :: Witherable FastList
 
-fromFoldable :: forall f. Foldable f => f ~> FastList
-fromFoldable = wrap <<< L.fromFoldable
+instance functorFastList :: Functor FastList where
+  map f (FastList xs) = FastList (map f xs)
 
-singleton :: forall a. a -> FastList a
-singleton = wrap <<< L.singleton
+instance filterableFastList :: Filterable FastList where
+  filter f (FastList xs) = FastList (filter f xs)
+  filterMap = mapMaybe
+  partition = partitionImpl
+  partitionMap
+    = runFn5 partitionMapImpl
+             isLeft
+             (unsafePartial fromLeft)
+             (unsafePartial fromRight)
 
-infix 8 range as ..
+foreign import map :: forall a b. (a -> b) -> List a -> List b
 
-range :: Int -> Int -> List Int
-range = wrap <<< L.range
+foreign import filter :: forall a. (a -> Boolean) -> List a -> List a
 
---TODO
---some :: forall f a. Alternative f => Lazy (f (FastList a)) => f a -> f (FastList a)
+mapMaybe :: forall a b. (a -> Maybe b) -> FastList a -> FastList b
+mapMaybe = runFn4 mapMaybeImpl isJust (unsafePartial fromJust)
 
---TODO
---someRec :: forall f a. MonadRec f => Alternative f => f a -> f (FastList a)
+foreign import mapMaybeImpl
+  :: forall a b
+   . Fn4 (Maybe b -> Boolean)
+         (Maybe b -> b)
+         (a -> Maybe b)
+         (FastList a)
+         (FastList b)
 
---TODO
---many :: forall f a. Alternative f => Lazy (f (List a)) => f a -> f (List a)
+foreign import partitionImpl
+  :: forall a
+   . (a -> Boolean)
+  -> FastList a
+  -> { no :: FastList a, yes :: FastList a }
+   
 
---TODO
---manyRec :: forall f a. MonadRec f => Alternative f => f a -> f (List a)
-
-null :: forall a. FastList a -> Boolean
-null = L.null <<< unwrap
-
-length :: forall a. FastList a -> Int
-length = L.length <<< unwrap
-
-snoc :: forall a. FastList a -> a -> FastList a
-snoc (FastList xs) x = FastList (L.snoc xs x)
-
-insert :: forall a. Ord a => a -> FastList a -> FastList a
-insert x (FastList xs) = FastList (L.insert x xs)
-
-insertBy :: forall a. (a -> a -> Ordering) -> a -> List a -> List a
-insertBy cmp x (FastList xs) = FastList (L.insertBy cmp x xs)
-
-head :: FastList ~> Maybe
-head = L.head <<< unwrap
-
-last :: FastList ~> Maybe
-last = L.last <<< unwrap
-
-tail :: forall a. FastList a -> Maybe (FastList a)
-tail (FastList xs) = FastList <$> L.tail xs
+foreign import partitionMapImpl
+  :: forall a l r
+   . Fn5 (Either l r -> Boolean)
+         (Either l r -> l)
+         (Either l r -> r)
+         (a -> Either l r)
+         (FastList a)
+         { left :: FastList l, right :: FastList r}
 
 foreign import zipWith :: forall a b c. Fn2 a b c -> FastList a -> FastList b -> FastList c
